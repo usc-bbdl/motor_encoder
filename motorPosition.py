9075bac7306d707799e0fa3865ceea9ec4581350
vesparza <vesparza@usc.edu>
2017-06-08 16:44:09 -0700

    Create motorPosition.py

--- motorPosition.py
+++ motorPosition.py
@@ -1,3 +1,5 @@
+# This class was created by using the Adafruit Library found at https://github.com/adafruit/Adafruit_Python_PCA9685
+
 from __future__ import division
 import time
 import translate 
@@ -6,21 +8,21 @@ import translate
 import Adafruit_PCA9685
 
 class Motor:
-	def __init__(self,freq,degMin,degMax,servo_min,servo_max):
+	def __init__(self,freq,degMin,degMax,servo_min,servo_max): # Constructor
 		# Initialise the PCA9685 using the default address (0x40).
 		self.pwm = Adafruit_PCA9685.PCA9685()
 		print "contructor worked trojan"
 		# Configure min and max servo pulse lengths
-		self.servo_min = int(servo_min)   # Min pulse length out of 409
+		self.servo_min = int(servo_min)   # Min pulse length out of 4096
 		self.servo_max =int(servo_max)  # Max pulse length out of 4096
-		self.degMin = degMin
-		self.degMax = degMax
+		self.degMin = degMin	# Min degrees out of 360
+		self.degMax = degMax	# Max degrees out of 360
 		# Set frequency to 60hz, good for servos.
 		self.pwm.set_pwm_freq(freq) 
 
-	def position(self,pins, posVector): #Both input arguments need to be explicit list.
+	def position(self,pins, posVector): #Both input arguments need to be explicit list. For example [50, 30, ..].
 		for c in range(len(posVector)):
 			#pulseRange maps degress into pulse length
 			d = translate.pulseRange(posVector[c], self.degMin, self.degMax, self.servo_min, self.servo_max)
-			self.pwm.set_pwm(pins[c],0,int(d))
+			self.pwm.set_pwm(pins[c],0,int(d)) 
 #			print "function worked trojan"
